c87d7159fd65a5e30715546d9c8cad047b8d277a
Julian Nicastro <julrodriguez@itba.edu.ar>
2017-06-19 00:46:22 -0300

    Add sleep to consumers

--- consumer/receive.py
+++ consumer/receive.py
@@ -1,16 +1,20 @@
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    time.sleep(5)
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+                      queue='task_queue')
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
